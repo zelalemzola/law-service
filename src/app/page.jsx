137684f65cfb4adc5b1d92154c222402c2b87ef0
zelalemzola <tzelalemtesfaye@gmail.com>
2024-09-16 09:33:49 +0300

    done

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -23,6 +23,11 @@ import {
 } from "@/components/ui/navigation-menu"
 import { ArrowRight, Facebook, Instagram, Linkedin, Menu, ReceiptText, Search, Twitter } from 'lucide-react';
 import { Button } from '@/components/ui/button';
+import GradualSpacing from '@/components/magicui/gradual-spacing'
+import BlurIn from '@/components/magicui/blur-in'
+import { inView } from 'framer-motion'
+import BlurFade from '@/components/magicui/blur-fade'
+import { FadeText } from '@/components/magicui/fade-text'
 const HomePage = () => {
   const services = [
   {
@@ -124,16 +129,33 @@ const HomePage = () => {
              
              </div>
             </div>
-        <div className='w-full h-screen landing pt-[15%] px-[5%] md:px-[20%]'>
-         <h1 className='hidden md:block md:text-6xl '>Solving your <br/>problems head-on</h1>
-         <h1 className='text-4xl md:hidden font-bold text-center my-[20%]'>Solving your problems head-on</h1>
-         <p className='text-xl md:w-[50%] my-6'>We provide you with direct and expert legal care so that you can resolve issues early and amicably.</p>
+        <div className='w-full h-screen landing pt-[15%] px-[5%] md:px-[10%] '>
+          <GradualSpacing
+             className="hidden text-start md:block md:text-5xl font-display  font-bold tracking-[-0.1em] -ml-[40%]   md:leading-[5rem]"
+             text={`Solving your problems head-on`}
+             
+           />
+          <GradualSpacing
+             className="text-3xl md:hidden mt-[30%] text-center font-bold   font-display   tracking-[-0.1em]   md:leading-[5rem]"
+             text={`Solving your problems head-on`}
+           />
+          <BlurIn
+          duration={0.50}
+      word="We provide you with direct and expert legal care so that you can resolve issues early and amicably."
+      className="text-xl md:w-[50%] my-6"
+    />
+         {/* <p className='text-xl md:w-[50%] my-6'>We provide you with direct and expert legal care so that you can resolve issues early and amicably.</p> */}
          <Link href='/contact' className='bg-primary py-3 px-6 rounded-xl shadow-md hover:shadow-2xl hover:bg-secondary text-white text-lg transition duration-700 ease-in-out '>Contact Us</Link>
         </div>
         <div className='w-full md:h-screen py-[10%] px-[10%] flex gap-20'>
          <div className='md:w-[60%] flex flex-col items-center justify-center gap-6'>
           <h1 className='text-primary text-4xl md:text-6xl'>About <span className='text-secondary'>Our Firm</span></h1>
-          <p className='text-lg text-center'>Sina  law firm is  established with the aim of serving clients needs. It majorly provides a quality legal advisory mediation and litigation service. Besed in Ethiopia. It established by prominent lawyer who served various corporate clients for over a decades.</p>
+          
+          <BlurFade delay={0.35} inView='true'>
+            <p className='text-lg text-center'>Sina  law firm is  established with the aim of serving clients needs. It majorly provides a quality legal advisory mediation and litigation service. Besed in Ethiopia. It established by prominent lawyer who served various corporate clients for over a decades.</p>
+          </BlurFade>
+
+          
           <Link href='/about' className='text-white bg-primary text-lg py-2 px-6 rounded-xl flex items-center gap-2 shadow-md hover:shadow-2xl hover:gap-4'>Learn More  <ArrowRight /></Link>
          </div>
          <div className='hidden md:flex items-center justify-center'>
@@ -170,14 +192,23 @@ const HomePage = () => {
         
         <div className='w-full md:h-screen py-[5%] md:py-[2%] px-[2%] flex flex-col md:flex-row gap-6 bg-[#4d4d4d]'>
           <div className="md:w-[20%] flex flex-col items-center justify-center gap-10">
-            <h1 className='text-4xl text-white text-center'>What We Focus On</h1>
+            <FadeText
+        className="text-4xl text-white text-center"
+        direction="left"
+        framerProps={{
+          show: { transition: { delay: 1 } },
+
+        }}
+        text="What We Focus On"
+      />
+           
             <Link href='/services' className='text-white py-4 px-8 rounded-xl shadow-xl bg-primary'>Services</Link>
           </div>
 
           <div className=' md:w-[80%] flex fle-col md:flex-row flex-wrap items-center justify-center gap-6'>
                           
-
-            <div className='md:w-[35%] bg-primary p-3 flex flex-col gap-3 rounded-xl'>
+            <BlurFade delay={0.25} inView='true'className='md:w-[35%] bg-primary p-3 flex flex-col gap-3 rounded-xl'>
+            <div >
               <h1 className='text-2xl text-white font-bold'>Mediation</h1>
               <p className='text-white '>
                  As a neutral third party we assist you in reaching a mutually acceptable resolution. focus on collaboration and communication allowing parties to express thier concern and interests.
@@ -204,7 +235,9 @@ const HomePage = () => {
                  </DrawerContent>
                </Drawer>
             </div>
-            <div className='md:w-[35%] bg-primary p-3 flex flex-col gap-3 rounded-xl'>
+           </BlurFade>
+            <BlurFade delay={0.30} inView='true'className='md:w-[35%] bg-primary p-3 flex flex-col gap-3 rounded-xl'>
+            <div >
               <h1 className='text-2xl text-white font-bold'>Contract</h1>
 
               <p className='text-white '>
@@ -232,7 +265,9 @@ const HomePage = () => {
                  </DrawerContent>
                </Drawer>
             </div>
-            <div className='md:w-[35%] bg-primary p-3 flex flex-col gap-3 rounded-xl'>
+            </BlurFade>
+             <BlurFade delay={0.35} inView='true'className='md:w-[35%] bg-primary p-3 flex flex-col gap-3 rounded-xl'>
+            <div >
               <h1 className='text-2xl text-white font-bold'>Commercial Law</h1>
               <p className='text-white '>
                  You gain a dedicated advocate who is commited to your success equipped with the expertise. And resources necessary to navigate the complexities of commercial legal matters effectively...
@@ -259,48 +294,101 @@ const HomePage = () => {
                  </DrawerContent>
                </Drawer>
             </div>
-            <div className='w-[35%] bg-primary p-3 hidden md:flex flex-col gap-3 rounded-xl lawfocus h-[45%]'>
+            </BlurFade>
+             <BlurFade delay={0.40} inView='true'className='w-[35%] bg-primary p-3 hidden md:flex flex-col gap-3 rounded-xl lawfocus h-[45%]'>
+            <div >
             
             </div>
-            
+            </BlurFade>
             
           </div>
         </div>
         <div className='w-full  py-[3%] px-[3%]'>
-          
-          <h1 className='text-5xl text-center text-primary font-bold my-[5%]'>Our Main Services</h1>
+          <div className='w-full text-center my-[5%]'>
+           <FadeText
+        className='text-5xl text-center text-primary font-bold '
+        direction="down"
+        framerProps={{
+          show: { transition: { delay: 0.21 } },
+        }}
+        text="Our Main Services"
+      />
+         </div>
           <div className="flex items-center justify-center flex-row flex-wrap gap-10">
-            <div className='bg-primary text-white px-6 py-3 text-lg rounded-xl'>Mediation</div>
-            <div className='bg-primary text-white px-6 py-3 text-lg rounded-xl'>Family Law</div>
+              <BlurFade delay={0.23} inView='true'>
+                <div className='bg-primary text-white px-6 py-3 text-lg rounded-xl'>Mediation</div>
+              </BlurFade>
+              <BlurFade delay={0.26} inView='true'>
+              <div className='bg-primary text-white px-6 py-3 text-lg rounded-xl'>Family Law</div>
+            </BlurFade>
+              <BlurFade delay={0.29} inView='true'>
             <div className='bg-primary text-white px-6 py-3 text-lg rounded-xl'>Employment Law</div>
-           <div className='bg-primary text-white px-6 py-3 text-lg rounded-xl'>Commercial Law</div>
+            </BlurFade>
+              <BlurFade delay={0.32} inView='true'>
+             <div className='bg-primary text-white px-6 py-3 text-lg rounded-xl'>Commercial Law</div>
+            </BlurFade>
+            <BlurFade delay={0.35} inView='true'>
             <div className='bg-primary text-white px-6 py-3 text-lg rounded-xl'>Succession</div>
+            </BlurFade>
+              <BlurFade delay={0.38} inView='true'>
             <div className='bg-primary text-white px-6 py-3 text-lg rounded-xl'>Tax</div>
+            </BlurFade>
+              <BlurFade delay={0.41} inView='true'>
             <div className='bg-primary text-white px-6 py-3 text-lg rounded-xl'>Contract</div>
+            </BlurFade>
+              <BlurFade delay={0.44} inView='true'>
             <div className='bg-primary text-white px-6 py-3 text-lg rounded-xl'>Criminal Law</div>
+            </BlurFade>
+              <BlurFade delay={0.47} inView='true'>
             <div className='bg-primary text-white px-6 py-3 text-lg rounded-xl'> Pro bono</div>
+            </BlurFade>
+              <BlurFade delay={0.50} inView='true'>
             <div className='bg-primary text-white px-6 py-3 text-lg rounded-xl'>Substantive legal training and research</div>
+            </BlurFade>
           </div>
           <Link  href='/services' className='mx-auto px-6 py-3 text-lg flex items-center gap-3 bg-primary w-fit text-white rounded-xl my-[3%] hover:bg-secondary transition duration-700 ease-in-out'>Learn More<ArrowRight/></Link>
         </div>
         <div className='w-full md:h-screen py-[10%] md:py-[3%] px-[3%] bg-primary flex flex-col gap-10 md:gap-0 md:flex-row'>
          <div className='md:w-[50%] flex items-center md:items-start justify-center gap-8 flex-col'>
-            <h1 className='text-white text-3xl font-bold'>Let's Talk</h1>
-            <p className='bg-white p-2 w-[80%] text-black text-center rounded-2xl '>Addis Ababa, Bole Atlas</p>
-            <p className='bg-white p-2 w-[80%] text-black text-center rounded-2xl '>0943902245</p>
-            <p className='bg-white p-2 w-[80%] text-black text-center rounded-2xl '>abrillawfirm@gmail.com</p>
+                  <FadeText
+              className='text-white text-3xl font-bold'
+              direction="left"
+              framerProps={{
+                show: { transition: { delay: 0.4 } },
+              }}
+              text="Let's Talk"
+            />
+              <BlurFade delay={0.44} inView='true' className='bg-white p-2 w-[80%] text-black text-center rounded-2xl '>
+            <p >Addis Ababa, Bole Atlas</p>
+            </BlurFade>
+              <BlurFade delay={0.48} inView='true' className='bg-white p-2 w-[80%] text-black text-center rounded-2xl '>
+            <p >0943902245</p>
+            </BlurFade>
+              <BlurFade delay={0.52} inView='true' className='bg-white p-2 w-[80%] text-black text-center rounded-2xl '>
+            <p >abrillawfirm@gmail.com</p>
+            </BlurFade>
+            <BlurFade delay={0.62} inView='true'>
             <Link href='/contact' className='bg-secondary p-3 w-fit text-white shadow-md  hover:shadow-2xl rounded-2xl text-center flex items-center gap-3 hover:bg-white hover:text-black transition duration-700 ease-in-out'>Contact Us<ArrowRight/></Link>
+            </BlurFade>
          </div>
          <div className='md:w-[50%] flex items-center md:items-start justify-center flex-col gap-10 '>
-             <h1 className='text-white text-3xl font-bold'>Business Hours</h1>
-             <div className='bg-white p-5 text-black flex flex-col items-start gap-5 rounded-xl w-[70%] text-center shadow-2xl'>
+             <FadeText
+            className='text-white text-3xl font-bold'
+              direction="right"
+              framerProps={{
+                show: { transition: { delay: 0.6 } },
+              }}
+              text=" Business Hours"
+            />
+               <BlurFade delay={0.63} inView='true'className='bg-white p-5 text-black flex flex-col items-start gap-5 rounded-xl w-[70%] text-center shadow-2xl'>
+          
               <p className='text-lg '>Monday to Friday</p>
               <p className='text-lg '>2:30 - 11:30LT</p>
               <p className='text-lg '>Saturday</p>
               <p className='text-lg '>2:30 - 5:30LT</p>
               <p className='text-lg '>We work on sundays for clients who only have appointments</p>
-              
-             </div>
+           
+             </BlurFade>
          </div>
         </div>
         <div className='w-full py-[5%] px-[5%]'>
