19877a6174a0e6940d5e5bf3399af3398444f7e6
zelalemzola <tzelalemtesfaye@gmail.com>
2024-11-19 14:41:05 +0300

    done

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -14,6 +14,7 @@ import {
   NavigationMenuList,
   NavigationMenuTrigger,
   NavigationMenuViewport,
+  
 } from "@/components/ui/navigation-menu"
 const AboutPage = () => {
         const services = [
